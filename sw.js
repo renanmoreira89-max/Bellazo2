d85d52c47e8cc7ca7359dac1cad55b30c6300208
renanmoreira89-max <renanmoreira89@gmail.com>
2026-07-05 12:47:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker para BellaZo - Suporte Offline e Sincronização
-const CACHE_NAME = 'bellazo-v10';
+const CACHE_NAME = 'bellazo-v1';
 const urlsToCache = [
   './',
   'index.html',
